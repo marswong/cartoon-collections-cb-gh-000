d3b37232a5c1a31f4cb8088579a47c82b1c62df2
Mars Wong <marswong618@gmail.com>
2018-01-26 03:48:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
 def roll_call_dwarves(arr)
-  arr.map { |x, i| print "#{i}.*#{x}" }
+  arr.collect.with_index { |x, i| print "#{i}.*#{x}" }
 end
 
 def summon_captain_planet(arr)
-  arr.collect { |x| "#{x.capitalize}!" }
+  arr.map { |x| "#{x.capitalize}!" }
 end
 
 def long_planeteer_calls(arr)
