cca90c280a20c46f228003466e70e41a1839b72f
Mars Wong <marswong618@gmail.com>
2018-01-26 03:42:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def summon_captain_planet(arr)
   arr.map { |x| "#{x.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  result = arr.find { |x| x.length > 4 }
+  result ? true : false
 end
 
 def find_the_cheese# code an argument here
